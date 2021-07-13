5938384b2e57ff0bbd4d50c1a7ed72819a1ed85e
kangangga <kangangga@users.noreply.github.com>
2021-07-13 21:37:35 +0000

    deploy: 33e7de86911255de7367cdc91d69af32c526f371

---
+++ _nuxt/static/1626212254/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Falik-net.github.io\u002F_nuxt\u002Fstatic\u002F1626212254",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Falik-net.github.io\u002F",assetsPath:"\u002Falik-net.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e267c1fb"}}}}(null));
